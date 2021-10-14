681fe1261d87cf10da3d9fb6101aa0f159f62901
thejaswin123 <thejas2002@yahoo.in>
2021-10-14 15:31:55 +0530

    initial

---
+++ about.php
@@ -0,0 +1,101 @@
+<?php $title="About Us"; include 'include/header.php'; ?>
+
+
+
+
+
+<div class="page-wrraper">
+
+	<section id="main" class="section bg-light text-center">
+		<div class="inner-content-main">
+			<div class="container">
+				<h1 class="text-capitalize mt-3 section-title text-white">ABOUT US</h1>
+				<h6 class="text-subheading m-4 text-white">There are following best types of crops in which we deals</h6>
+			</div>
+		</div>
+	</section>
+
+
+
+	<section id="how-it-work" class="padding-3  text-center bg-01">
+
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="all-informations">
+						<div class="single-info  p-5 bg-02">
+							<div class="row align-items-center">
+								<div class="col-lg-2">
+									<div class="info-image-container">
+										<div class="image-div">
+											<img src="images/search-htw.png" alt="">
+										</div>
+									</div>
+								</div>
+								<div class="col-lg-10">
+									<div class="single-heading">
+										RICE
+										<hr>
+									</div>
+									<div class="single-detail">
+										Rice is the most important grain with regard to human nutrition and caloric intake, providing more than one-fifth of the calories consumed worldwide by humans.[2] There are many varieties of rice and culinary preferences tend to vary regionally. Rice cultivation is well-suited to countries and regions with low labor costs and high rainfall, as it is labor-intensive to cultivate and requires ample water. However, rice can be grown practically anywhere, even on a steep hill or mountain area with the use of water-controlling terrace systems.Although its parent species are native to Asia and certain parts of Africa, centuries of trade and exportation have made it commonplace in many cultures worldwide
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="single-info  p-5 bg-02">
+							<div class="row align-items-center">
+								<div class="col-lg-10">
+									<div class="single-heading">
+										Wheat
+										<hr>
+									</div>
+									<div class="single-detail">
+										Wheat is grown on more land area than any other food crop. World trade in wheat is greater than for allother crops combined. Wheat is an important source of carbohydrates.[9] Globally, it is the leading source of vegetal protein in human food, having a protein content of about 13%, which is relatively high compared to other major cereals [11] but relatively low in protein quality for supplying essential amino acids.[12][13] When eaten as the whole grain, wheat is a source of multiple nutrients and dietary fiber.[9]
+									</div>
+								</div>
+								<div class="col-lg-2">
+									<div class="info-image-container">
+										<div class="image-div">
+											<img src="images/info-htw.png" alt="">
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="single-info  p-5 bg-02">
+							<div class="row align-items-center">
+								<div class="col-lg-2">
+									<div class="info-image-container">
+										<div class="image-div">
+											<img src="images/money-htw.png" alt="">
+										</div>
+									</div>
+								</div>
+								<div class="col-lg-10">
+									<div class="single-heading">
+										Cotton
+										<hr>
+									</div>
+									<div class="single-detail">
+										The nation is largely dependent on the cotton industry and its related textile sector, and the crop has been given a principal status in the country. Cotton is grown as an industrial crop in 15% of the nation's land during the monsoon months of May to August, known as the kharif period, and is grown at a smaller scale between February and April. Pakistan occupied the fourth position among the cotton growers of the world, the first three being China, India and the United States, in that order.[2] In respect of exports of raw cotton, Pakistan holds third position, and is the fourth in consumption (about 30 and 40 per cent of its production). It is the largest exporter of cotton yarn.[3]
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</section>
+
+	<section id="join-us" class="padding-3 text-center bg-02">
+		<h2 class="section-title text-white">Join Us</h2>
+		<div class="buttons mt-3">
+			<a href="farmer-registration.php" class="btn btn-primary text-capitalize">Become a Farmer!</a>
+			<a href="supplier-registration.php" class="btn btn-primary text-capitalize">Become a Supplier!</a>
+		</div>
+	</section>
+
+	<?php include 'include/footer.php'; ?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php $title="Home"; include 'include/header.php'; ?>
+
+<div class="page-wrraper">
+    <section id="main" class="section bg-light text-center text-white">
+        <div class="inner-content-main">
+            <div class="container">
+                <h1 class="text-capitalize mt-3">Welcome to (FAWS)</h1>
+
+                <h6 class="text-subheading m-4">Farming Assistant Web Service</h6>
+
+                <div class="buttons mt-5">
+                    <a href="admin/" class="btn btn-primary text-capitalize">ADMIN</a>
+                    <a href="farmer/" class="btn btn-primary text-capitalize">FARMER</a>
+                    <a href="supplier/" class="btn btn-primary text-capitalize">SUPPLIER</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php include 'include/footer.php'; ?>
+</div>
